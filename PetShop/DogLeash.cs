e62d43e1ac8d2e2aa956ec469d59112213d8a9ce
John W. Stokes, Jr. <tkdjohn71@gmail.com>
2024-11-13 21:42:57 -0500

    initial

---
+++ PetShop/DogLeash.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop
+{
+    public class DogLeash : Product
+    {
+        public int LengthInches { get; set; }
+        public string Material { get; set; } = "";
+    }
+}
---
+++ PetShop/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop
+{
+    public class Product
+    {
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public int Qty { get; set; } = 0;
+        public decimal Price { get; set; } = 0.0M;
+    }
+}
