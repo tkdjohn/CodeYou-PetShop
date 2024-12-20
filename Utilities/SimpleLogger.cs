58eb3929cb9d2e4d1816ecf34f02a8e11953a8fc
John W. Stokes, Jr. <tkdjohn71@gmail.com>
2024-12-20 13:42:41 -0500

    ?

--- Utilities/SimpleLogger.cs
+++ Utilities/SimpleLogger.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Utilities {
+﻿namespace Utilities {
     public static class SimpleLogger {
         public static void LogException(Exception ex) {
             // for now we will just write it to error console
