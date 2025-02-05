e9550dd6bfd1d5f94f9a39f5d0fdf8aed3e12a06
John W. Stokes, Jr. <tkdjohn71@gmail.com>
2025-02-04 21:43:28 -0500

    Petshop 10 YES 10 not 9 MAJOR REFACTOR

---
+++ PetShop.Data/IPetShopDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop.DomainEntities;
+
+namespace PetShop.Data {
+    public interface IPetShopDbContext {
+        string DbPath { get; }
+        DbSet<Order> Orders { get; }
+        DbSet<Product> Products { get; }
+
+        Task SaveChangesAsync();
+    }
+}
